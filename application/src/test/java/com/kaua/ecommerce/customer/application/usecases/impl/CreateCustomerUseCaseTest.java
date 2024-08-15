592790e971d8b636dc3767f78c55538950e6047a
Kauã Pereira <zkauacodes@hotmail.com>
2024-08-15 08:49:04 -0300

    test(create-customer-and-idp-user): add the create customer usecase and create idp user usecase abstractions and implementation tests

---
+++ application/src/test/java/com/kaua/ecommerce/customer/application/usecases/impl/CreateCustomerUseCaseTest.java
@@ -0,0 +1,83 @@
+package com.kaua.ecommerce.customer.application.usecases.impl;
+
+import com.kaua.ecommerce.customer.application.UseCaseTest;
+import com.kaua.ecommerce.customer.application.exceptions.UseCaseInputCannotBeNullException;
+import com.kaua.ecommerce.customer.application.repositories.CustomerRepository;
+import com.kaua.ecommerce.customer.application.usecases.customer.impl.DefaultCreateCustomerUseCase;
+import com.kaua.ecommerce.customer.application.usecases.customer.inputs.CreateCustomerInput;
+import com.kaua.ecommerce.lib.domain.exceptions.DomainException;
+import com.kaua.ecommerce.lib.domain.utils.IdentifierUtils;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+
+import static org.mockito.AdditionalAnswers.returnsFirstArg;
+
+class CreateCustomerUseCaseTest extends UseCaseTest {
+
+    @Mock
+    private CustomerRepository customerRepository;
+
+    @InjectMocks
+    private DefaultCreateCustomerUseCase createCustomerUseCase;
+
+    @Test
+    void givenAValidValues_whenCallCreateCustomer_thenCustomerShouldBeCreated() {
+        final var aUserId = IdentifierUtils.generateNewUUID();
+        final var aCustomerId = IdentifierUtils.generateNewUUID();
+        final var aFirstName = "John";
+        final var aLastName = "Doe";
+        final var aEmail = "testes.tess@tess.com";
+
+        final var aInput = new CreateCustomerInput(aUserId, aCustomerId, aFirstName, aLastName, aEmail);
+
+        Mockito.when(customerRepository.existsByEmail(aEmail)).thenReturn(false);
+        Mockito.when(customerRepository.save(Mockito.any())).thenAnswer(returnsFirstArg());
+
+        final var aOutput = this.createCustomerUseCase.execute(aInput);
+
+        Assertions.assertEquals(aCustomerId, aOutput.customerId());
+        Assertions.assertEquals(aUserId, aOutput.userId());
+
+        Mockito.verify(customerRepository, Mockito.times(1)).existsByEmail(aEmail);
+        Mockito.verify(customerRepository, Mockito.times(1)).save(Mockito.any());
+    }
+
+    @Test
+    void givenAnExistsEmail_whenCallCreateCustomer_thenShouldThrowDomainException() {
+        final var aUserId = IdentifierUtils.generateNewUUID();
+        final var aCustomerId = IdentifierUtils.generateNewUUID();
+        final var aFirstName = "John";
+        final var aLastName = "Doe";
+        final var aEmail = "testes.tess@tess.com";
+
+        final var expectedErrorMessage = "Email already exists";
+
+        final var aInput = new CreateCustomerInput(aCustomerId, aUserId, aFirstName, aLastName, aEmail);
+
+        Mockito.when(customerRepository.existsByEmail(aEmail)).thenReturn(true);
+
+        final var aException = Assertions.assertThrows(DomainException.class,
+                () -> this.createCustomerUseCase.execute(aInput));
+
+        Assertions.assertEquals(expectedErrorMessage, aException.getMessage());
+
+        Mockito.verify(customerRepository, Mockito.times(1)).existsByEmail(aEmail);
+        Mockito.verify(customerRepository, Mockito.times(0)).save(Mockito.any());
+    }
+
+    @Test
+    void givenAnInvalidNullInput_whenCallCreateCustomer_thenShouldThrowUseCaseInputCannotBeNullException() {
+        final var expectedErrorMessage = "Input to %s cannot be null".formatted(DefaultCreateCustomerUseCase.class.getSimpleName());
+
+        final var aException = Assertions.assertThrows(UseCaseInputCannotBeNullException.class,
+                () -> this.createCustomerUseCase.execute(null));
+
+        Assertions.assertEquals(expectedErrorMessage, aException.getMessage());
+
+        Mockito.verify(customerRepository, Mockito.times(0)).existsByEmail(Mockito.any());
+        Mockito.verify(customerRepository, Mockito.times(0)).save(Mockito.any());
+    }
+}
---
+++ application/src/test/java/com/kaua/ecommerce/customer/application/usecases/impl/CreateIdpUserUseCaseTest.java
@@ -0,0 +1,57 @@
+package com.kaua.ecommerce.customer.application.usecases.impl;
+
+import com.kaua.ecommerce.customer.application.UseCaseTest;
+import com.kaua.ecommerce.customer.application.exceptions.UseCaseInputCannotBeNullException;
+import com.kaua.ecommerce.customer.application.gateways.IdentityProviderGateway;
+import com.kaua.ecommerce.customer.application.usecases.customer.impl.DefaultCreateIdpUserUseCase;
+import com.kaua.ecommerce.customer.application.usecases.customer.inputs.CreateIdpUserInput;
+import com.kaua.ecommerce.customer.domain.customer.idp.UserId;
+import com.kaua.ecommerce.lib.domain.utils.IdentifierUtils;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+
+class CreateIdpUserUseCaseTest extends UseCaseTest {
+
+    @Mock
+    private IdentityProviderGateway identityProviderGateway;
+
+    @InjectMocks
+    private DefaultCreateIdpUserUseCase createIdpUserUseCase;
+
+    @Test
+    void givenAValidValues_whenCallCreateIdpUser_thenIdpUserShouldBeCreated() {
+        final var aCustomerId = IdentifierUtils.generateNewUUID();
+        final var aFirstName = "John";
+        final var aLastName = "Doe";
+        final var aEmail = "testes.tess@tess.com";
+        final var aPassword = "1234567Ab*";
+
+        final var expectedUserId = new UserId(IdentifierUtils.generateNewUUID());
+
+        final var aInput = new CreateIdpUserInput(aCustomerId, aFirstName, aLastName, aEmail, aPassword);
+
+        Mockito.when(identityProviderGateway.create(Mockito.any())).thenReturn(expectedUserId);
+
+        final var aOutput = this.createIdpUserUseCase.execute(aInput);
+
+        Assertions.assertNotNull(aOutput);
+        Assertions.assertEquals(expectedUserId, aOutput.idpUserId());
+
+        Mockito.verify(identityProviderGateway, Mockito.times(1)).create(Mockito.any());
+    }
+
+    @Test
+    void givenAnInvalidNullInput_whenCallCreateIdpUser_thenShouldThrowUseCaseInputCannotBeNullException() {
+        final var expectedErrorMessage = "Input to %s cannot be null".formatted(DefaultCreateIdpUserUseCase.class.getSimpleName());
+
+        final var aException = Assertions.assertThrows(UseCaseInputCannotBeNullException.class,
+                () -> this.createIdpUserUseCase.execute(null));
+
+        Assertions.assertEquals(expectedErrorMessage, aException.getMessage());
+
+        Mockito.verify(identityProviderGateway, Mockito.times(0)).create(Mockito.any());
+    }
+}
