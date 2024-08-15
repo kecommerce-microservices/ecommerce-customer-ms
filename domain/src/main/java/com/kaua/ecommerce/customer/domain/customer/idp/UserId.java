447acdbd25894cb9ed5eced563753ac1a30d27d7
Kauã Pereira <zkauacodes@hotmail.com>
2024-08-15 09:31:48 -0300

    fix(sonar): fix sonar issues

--- domain/src/main/java/com/kaua/ecommerce/customer/domain/customer/idp/UserId.java
+++ domain/src/main/java/com/kaua/ecommerce/customer/domain/customer/idp/UserId.java
@@ -6,10 +6,6 @@ import java.util.UUID;
 
 public record UserId(UUID value) implements Identifier<UUID> {
 
-//    public UserId {
-//        this.assertArgumentNotNull(value, "id", "should not be null");
-//    }
-
     public static UserId empty() {
         return new UserId(null);
     }
