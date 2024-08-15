7d18ed3b584d9eb528806ea3eebe3e0813f7b659
Kauã Pereira <zkauacodes@hotmail.com>
2024-08-15 08:35:13 -0300

    feat(document): add the document value objects

---
+++ domain/src/main/java/com/kaua/ecommerce/customer/domain/person/DocumentFactory.java
@@ -0,0 +1,18 @@
+package com.kaua.ecommerce.customer.domain.person;
+
+import com.kaua.ecommerce.lib.domain.exceptions.DomainException;
+import com.kaua.ecommerce.lib.domain.utils.CnpjUtils;
+import com.kaua.ecommerce.lib.domain.utils.CpfUtils;
+
+public final class DocumentFactory {
+
+    private DocumentFactory() {}
+
+    public static Document create(final String documentNumber, final String documentType) {
+        return switch (documentType) {
+            case Document.Cpf.DOCUMENT_TYPE -> new Document.Cpf(CpfUtils.cleanCpf(documentNumber));
+            case Document.Cnpj.DOCUMENT_TYPE -> new Document.Cnpj(CnpjUtils.cleanCnpj(documentNumber));
+            default -> throw DomainException.with("Invalid document type");
+        };
+    }
+}
