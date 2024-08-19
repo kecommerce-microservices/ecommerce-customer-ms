d09d6f7db400c596a031d7f7dc8fa34f84a380fc
Kauã Pereira <zkauacodes@hotmail.com>
2024-08-19 11:11:45 -0300

    test(abstract-rest-client): add the bulkhead and circuit breaker helpers methods

--- infrastructure/src/test/java/com/kaua/ecommerce/customer/AbstractRestClientTest.java
+++ infrastructure/src/test/java/com/kaua/ecommerce/customer/AbstractRestClientTest.java
@@ -3,6 +3,11 @@ package com.kaua.ecommerce.customer;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.tomakehurst.wiremock.client.WireMock;
 import com.kaua.ecommerce.customer.infrastructure.configurations.WebServerConfig;
+import com.kaua.ecommerce.customer.infrastructure.gateways.AuthServerIdentityProviderClient;
+import io.github.resilience4j.bulkhead.BulkheadRegistry;
+import io.github.resilience4j.circuitbreaker.CircuitBreaker;
+import io.github.resilience4j.circuitbreaker.CircuitBreakerRegistry;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,19 +15,51 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
 import org.springframework.test.context.ActiveProfiles;
 
+import java.util.List;
+
 @ActiveProfiles("test-integration")
 @AutoConfigureWireMock(port = 0)
 @SpringBootTest(classes = {WebServerConfig.class})
 @Tag("integrationTest")
 public abstract class AbstractRestClientTest {
 
+    protected static final String USERS = AuthServerIdentityProviderClient.NAMESPACE_NAME;
+
     @Autowired
     private ObjectMapper mapper;
+
+    @Autowired
+    private BulkheadRegistry bulkheadRegistry;
+
+    @Autowired
+    private CircuitBreakerRegistry circuitBreakerRegistry;
 
     @BeforeEach
     void beforeEach() {
         WireMock.reset();
         WireMock.resetAllRequests();
+        List.of(USERS).forEach(this::resetFaultTolerance);
+    }
+
+    protected void checkCircuitBreakerState(final String name, final CircuitBreaker.State expectedState) {
+        final var cb = circuitBreakerRegistry.circuitBreaker(name);
+        Assertions.assertEquals(expectedState, cb.getState());
+    }
+
+    protected void transitionToOpenState(final String name) {
+        circuitBreakerRegistry.circuitBreaker(name).transitionToOpenState();
+    }
+
+    protected void transitionToClosedState(final String name) {
+        circuitBreakerRegistry.circuitBreaker(name).transitionToClosedState();
+    }
+
+    protected void acquireBulkheadPermission(final String name) {
+        bulkheadRegistry.bulkhead(name).acquirePermission();
+    }
+
+    protected void releaseBulkheadPermission(final String name) {
+        bulkheadRegistry.bulkhead(name).releasePermission();
     }
 
     protected String writeValueAsString(final Object obj) {
@@ -31,5 +68,9 @@ public abstract class AbstractRestClientTest {
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private void resetFaultTolerance(final String name) {
+        circuitBreakerRegistry.circuitBreaker(name).reset();
     }
 }
