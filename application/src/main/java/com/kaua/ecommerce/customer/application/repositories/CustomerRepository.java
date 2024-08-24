add290a25da5ec200d8dd4ddb4d2f6be3ece4c01
Kauã Pereira <zkauacodes@hotmail.com>
2024-08-24 15:19:27 -0300

    feat(customer-repository): add the exists by document and customer of id methods

--- application/src/main/java/com/kaua/ecommerce/customer/application/repositories/CustomerRepository.java
+++ application/src/main/java/com/kaua/ecommerce/customer/application/repositories/CustomerRepository.java
@@ -3,11 +3,17 @@ package com.kaua.ecommerce.customer.application.repositories;
 import com.kaua.ecommerce.customer.domain.customer.Customer;
 import com.kaua.ecommerce.customer.domain.customer.CustomerId;
 
+import java.util.Optional;
+
 public interface CustomerRepository {
 
     CustomerId nextId();
 
     boolean existsByEmail(String email);
 
+    boolean existsByDocument(String documentNumber);
+
     Customer save(Customer customer);
+
+    Optional<Customer> customerOfId(CustomerId customerId);
 }
