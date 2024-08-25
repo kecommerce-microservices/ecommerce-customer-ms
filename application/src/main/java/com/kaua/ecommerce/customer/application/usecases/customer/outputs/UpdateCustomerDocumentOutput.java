ab70f72e40fc6370df73da52e65f2f6184d2f587
Kauã Pereira <zkauacodes@hotmail.com>
2024-08-25 10:26:56 -0300

    feat(update-customer-document-output): add the throw internal error exception on create object with null document

--- application/src/main/java/com/kaua/ecommerce/customer/application/usecases/customer/outputs/UpdateCustomerDocumentOutput.java
+++ application/src/main/java/com/kaua/ecommerce/customer/application/usecases/customer/outputs/UpdateCustomerDocumentOutput.java
@@ -1,10 +1,14 @@
 package com.kaua.ecommerce.customer.application.usecases.customer.outputs;
 
 import com.kaua.ecommerce.customer.domain.customer.Customer;
+import com.kaua.ecommerce.customer.domain.person.Document;
+import com.kaua.ecommerce.lib.domain.exceptions.InternalErrorException;
 
 public record UpdateCustomerDocumentOutput(String customerId, String documentType) {
 
     public UpdateCustomerDocumentOutput(final Customer aCustomer) {
-        this(aCustomer.getId().value().toString(), aCustomer.getDocument().get().type());
+        this(aCustomer.getId().value().toString(), aCustomer.getDocument().map(Document::type)
+                .orElseThrow(() -> InternalErrorException.with("On creating %s, document type is null"
+                        .formatted(UpdateCustomerDocumentOutput.class.getSimpleName()))));
     }
 }
