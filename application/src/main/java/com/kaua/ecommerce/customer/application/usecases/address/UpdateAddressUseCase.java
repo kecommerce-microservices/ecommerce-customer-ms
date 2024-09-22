035d56de57f6b73f39c3a9fdb54cfb4b57917d26
Kauã Pereira <zkauacodes@hotmail.com>
2024-09-22 12:25:08 -0300

    feat(update-address-usecase): add the update address usecase abstraction and implementation

---
+++ application/src/main/java/com/kaua/ecommerce/customer/application/usecases/address/UpdateAddressUseCase.java
@@ -0,0 +1,9 @@
+package com.kaua.ecommerce.customer.application.usecases.address;
+
+import com.kaua.ecommerce.customer.application.UseCase;
+import com.kaua.ecommerce.customer.application.usecases.address.inputs.UpdateAddressInput;
+import com.kaua.ecommerce.customer.application.usecases.address.outputs.UpdateAddressOutput;
+
+public abstract class UpdateAddressUseCase extends
+        UseCase<UpdateAddressInput, UpdateAddressOutput> {
+}
